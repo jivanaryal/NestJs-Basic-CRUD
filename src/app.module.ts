1e2a91b332b5e6ba991d3c0d790b26b6dde03fb5
jivanaryal <jeevanaryal7947@gmail.com>
2025-02-07 00:07:09 +0545

    process.env uses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductModule } from './products/products.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ProductModule } from './products/products.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
+console.log(process.env.PORT, process.env.DATABASE, process.env.USERNAME);
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // This ensures .env variables are available everywhere
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      username: 'jivan',
-      password: 'Jivan@admin123',
-      database: 'ecommerce',
-      port: 3309,
-      logging: false,
+      host: process.env.HOST,
+      username: process.env.NAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      port: Number(process.env.PORT), // Convert port to number
       autoLoadEntities: true,
       synchronize: true,
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +15,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.APP_PORT ?? 3000;
   await app.listen(port);
   console.log(`🚀 Server running on http://localhost:${port}`);
 }
