42bce7ece00d28307c7922e457749c21d950f5ae
Jivan Aryal <104553708+jivanaryal@users.noreply.github.com>
2025-02-06 23:29:47 +0545

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
-  console.log(`🚀 Server running on http://localhost:${port}`);
+  console.log(` Server running on http://localhost:${port}`);
 }
 bootstrap();
